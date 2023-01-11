a62ff97b0a0116eb8245af728ce6c8db7f8d3185
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-01-11 11:29:06 +0900

    Update Hashing.py

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,6 +1,8 @@
+from string import ascii_lowercase
+alp=list(ascii_lowercase)
 a=int(input())
 b=input()
 s=0
-for k in range(a):
-    s+=(ord(b[k])-96)*(31**k)
+for k in range(len(b)):
+    s+=(alp.index(b[k])+1)*(31**k)
 print(s)    
