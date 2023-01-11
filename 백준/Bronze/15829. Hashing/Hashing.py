c094a9bf98f42b4c712dba36642701783fcd47a6
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-01-11 11:02:11 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 34816 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,14 +1,8 @@
 from string import ascii_lowercase
-
 alp=list(ascii_lowercase)
-x={}
-for i in range(26):
-    x[alp[i]]=i+1
-
 a=int(input())
 b=input()
 s=0
-for k in range(len(b)):
-    s+=x[b[k]]*(31**k)
-
+for k in range(a):
+    s+=(alp.index(b[k])+1)*(31**k)
 print(s)    
