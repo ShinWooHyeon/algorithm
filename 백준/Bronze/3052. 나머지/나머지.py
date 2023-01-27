e2c6b06f49997523b2ea6c7d2851509e812aad7e
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-01-27 14:12:42 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,7 +1,5 @@
+import sys
 x=[]
 for i in range(10):
-  x.append(int(input()))
-y=[]
-for i in x:
-    y.append(i%42)
-print(len(set(y)))
+  x.append(int(sys.stdin.readline())%42)
+print(len(set(x)))
