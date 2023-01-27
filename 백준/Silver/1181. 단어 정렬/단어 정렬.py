a389825bb5b225912f4e4889a97c05ebf7f2544f
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-01-27 14:17:05 +0900

    [Silver V] Title: 단어 정렬, Time: 72 ms, Memory: 35192 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -2,11 +2,9 @@ import sys
 n = int(sys.stdin.readline())
 x=[None]*n
 for i in range (n):
-    a=sys.stdin.readline().strip('\n')
-    x[i]=a
-k=list(set(x))
-k.sort()
-k.sort(key=len)
+    x[i]=sys.stdin.readline().strip('\n')
+k=list(set(x)) 
+k.sort()         
+k.sort(key=len)  
 for i in k:
     print(i)
-                        
