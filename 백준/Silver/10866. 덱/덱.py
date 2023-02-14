264ca40b0207bb74c5d56fb05d39c2db1e81f8d1
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-02-14 15:31:04 +0900

    [Silver IV] Title: 덱, Time: 76 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+def deque_func():
+    n=int(input())
+    deque_list=deque([])
+    for i in range (n):
+        order=input().split()
+        if len(order)==2:
+            if order[0]=='push_front':
+                deque_list.appendleft(int(order[1]))
+            else:
+                deque_list.append(int(order[1]))
+        else:
+            if order[0]=='pop_front':
+                if len(deque_list)!=0:
+                    print(deque.popleft(deque_list))
+                else:
+                    print(-1)
+            elif order[0]=='pop_back':
+                if len(deque_list)!=0:
+                    print(deque.pop(deque_list))
+                else:
+                    print(-1)
+            elif order[0]=='size':
+                print(len(deque_list)) 
+            elif order[0]=='empty':
+                if len(deque_list)==0:
+                    print(1)
+                else:
+                    print(0)
+            elif order[0]=='front':
+                if len(deque_list)!=0:
+                    print(deque_list[0])
+                else:
+                    print(-1)
+            else:
+                if len(deque_list)!=0:
+                    print(deque_list[-1])
+                else:
+                    print(-1)
+deque_func()
