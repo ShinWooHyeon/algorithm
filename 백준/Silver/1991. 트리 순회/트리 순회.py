ee0404a90d64fd2e233e5923673ce9ee42ed2721
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-08-27 23:29:42 +0900

    [Silver I] Title: 트리 순회, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,36 +1,30 @@
 import sys
- 
-N = int(sys.stdin.readline().strip())
+input=sys.stdin.readline
+N = int(input().strip())
 tree = {}
  
 for n in range(N):
-    root, left, right = sys.stdin.readline().strip().split()
+    root, left, right = input().strip().split()
     tree[root] = [left, right]
  
- 
-def preorder(root):
-    if root != '.':
-        print(root, end='')  # root
-        preorder(tree[root][0])  # left
-        preorder(tree[root][1])  # right
- 
- 
-def inorder(root):
-    if root != '.':
-        inorder(tree[root][0])  # left
-        print(root, end='')  # root
-        inorder(tree[root][1])  # right
- 
- 
-def postorder(root):
-    if root != '.':
-        postorder(tree[root][0])  # left
-        postorder(tree[root][1])  # right
-        print(root, end='')  # root
- 
- 
-preorder('A')
+def pre_order(root):
+    if root !='.':
+        print(root,end="")
+        pre_order(tree[root][0])
+        pre_order(tree[root][1])
+def in_order(root):
+    if root !='.':
+        in_order(tree[root][0])
+        print(root,end="")
+        in_order(tree[root][1])
+def post_order(root):
+    if root !='.':
+        post_order(tree[root][0])
+        post_order(tree[root][1])
+        print(root,end="")
+
+pre_order('A')
 print()
-inorder('A')
+in_order('A')
 print()
-postorder('A')
+post_order('A')
