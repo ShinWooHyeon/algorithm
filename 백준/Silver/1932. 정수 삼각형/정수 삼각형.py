2ad2f9e53c351ace5310205c4fce257ec41b5e3b
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-09-04 19:25:54 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 36532 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,14 +1,24 @@
-n=int(input())
-d=[]
-for i in range(n):
-  d.append(list(map(int, input().split())))
-
-for i in range(1,n):
-  for j in range(len(d[i])):
-    if j==0:
-      d[i][j]=d[i][j]+d[i-1][j]
-    elif j==len(d[i])-1: 
-      d[i][j]=d[i][j]+d[i-1][j-1]
+# 정수삼각형
+n = int(input())
+dp=[]
+for i in range (n):
+    if i != n-1:
+        line=[0] * (n-1-i)
     else:
-      d[i][j]=max(d[i-1][j-1],d[i-1][j])+d[i][j]
-print(max(d[n-1]))
+        line=[]
+    line.extend(list(map(int, input().split()))) 
+    dp.append(line)
+
+# 바텀 업 방식으로 dp 진행
+
+for i in range (1, n):
+    for j in range(n):
+        # 바로위 혹은 대각선 오른쪽 위에서 내려오는 경우 밖에 없다 
+        if j == n-1:
+            right_up = 0
+        else:
+            right_up = dp[i-1][j + 1]
+        up = dp[i-1][j]
+        dp[i][j] = max(right_up, up) + dp[i][j]
+
+print(max(dp[n-1]))       
