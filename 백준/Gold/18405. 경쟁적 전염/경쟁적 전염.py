5e38ce04e33abae33a4bf8243b2c81122e6d4e14
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-10-10 15:24:39 +0900

    [Gold V] Title: 경쟁적 전염, Time: 108 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,50 +1,47 @@
-# 경쟁적 전염
-# 시험관 크기랑 바이러스 개수 입력받기
 from collections import deque
-n , k = map(int, input().split())
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-# 시험관 입력받기
-graph=[]
-virus_p=[]
-for i in range (n):
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+# 바이러스 맵을 입력받고
+vmap = []
+virus_point= []
+
+for i in range (N) :
     line = list(map(int, input().split()))
-    graph.append(line)
-    # 바이러스 번호, x,y  저장한다
-    for j in range (n):
-        if line[j] !=0:
-            virus_p.append((line[j], i, j))
-s, ans_x, ans_y = map(int, input().split())
-# 낮은 순서대로 해결하기 위해 큐에 입력한다 
-virus_p.sort(key = lambda x : x[0])
-
-def bfs (graph, virus_p):
-    queue= deque()
-    # 바이러스 번호 순서대로 큐에 삽입
-    for i in virus_p:
-        queue.append(i)
-    if s == 0:
-        return graph[ans_x-1][ans_y-1]
-    
+    vmap.append(line)
+    for j in range (N):
+        if line[j] != 0:
+            virus_point.append((line[j], i, j))
+# 작은순서대로 virus_start를 정렬한다 
+virus_point.sort(key = lambda x: x[0])
+# nx, ny 지정
+nx = [1, -1, 0, 0]
+ny = [0, 0, 1, -1]
+S, X, Y = map(int, input().split())
+# 큐에 초기 바이러스 순서대로 삽입해야한다 
+def bfs(vmap, virus_point):
+    virus_start = deque(virus_point)
+    if S == 0:
+        return vmap[X-1][Y-1]
     else:
-        t=0
-        while queue:
-            cycle = len(queue)
-            for a in range (cycle):
-            
-                v, x, y = queue.popleft() # 바이러스번호와 좌표 정보 받기
-                for i in range (4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if nx >= 0 and nx <n and ny >= 0 and ny < n :
-                        if graph[nx][ny] == 0 :
-                            graph[nx][ny] = v
-                            queue.append((v, nx, ny))
-            
-            t+=1
-            if t == s:
-                return graph[ans_x-1][ans_y-1]
-        if s > t:
-            return graph[ans_x-1][ans_y-1]
+        t = 0
+        while virus_start:
+            for i in range(len(virus_start)):
+                vn, a, b = virus_start.popleft()
+                for j in range (4) : 
+                    na = a + nx[j]
+                    nb = b + ny[j]
+                    if 0 <= na  < N and 0 <= nb <N and vmap[na][nb] == 0:
+                        vmap[na][nb] = vn
+                        virus_start.append((vn, na, nb)) 
+            t += 1
+            if t == S:
+                return vmap[X-1][Y-1]
+        # While문을 다 돌았는데도 즉 이미 바이러스 다 퍼져서 시간 덜 되었어도 종료되면
+        if t< S:
+            return vmap[X-1][Y-1]
 
-print(bfs(graph, virus_p))
+print(bfs(vmap, virus_point))
+#
+#def bfs_virus(start, S) :
+    
