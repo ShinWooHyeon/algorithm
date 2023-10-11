c04d28e1f20b9849aee022b62d541ac512478576
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-10-11 16:52:30 +0900

    [Gold IV] Title: 연구소, Time: 2476 ms, Memory: 175692 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,54 +1,65 @@
+# itertools를 사용하지 않고 조합으로 찾아보자
+# 벽 설치하고 퍼져야한다
+# 일단 맵을 입력받고 ,벽을 설치할 수 있는 경우들에 대해서 deepcopy 한 후 설치해서 
+from copy import deepcopy
 from collections import deque
-import copy
-n , m = map(int, input().split())
-graph = [] # 초기 맵
+n, m = map(int, input().split())
+# 그래프를 입력 받으면서 조합할 빈칸들의 리스트를 만들어야 한다 
+virus_map = []
+blank_point = []
+virus_point = []
+# bfs를 할때 사용할 방향을 지정해준다
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
+for i in range (n):
+    line = list(map(int, input().split()))
+    virus_map.append(line)
+    for j in range (m):
+        if line[j] == 0:
+            blank_point.append((i, j))
+        if line[j] == 2:
+            virus_point.append((i, j))
+def comb (array, n):
+    result = []
+    if n == 0:
+        return [[]]
+    for i in range(len(array)):
+        elem = array[i]
+        for j in comb(array[ :i] + array[i + 1 :], n - 1) :
+            result.append([elem] + j)  
+    return result
+# 조합 함수를 선언하고 설치가능한 벽의 경우들을 후보리스트로 만든다
+cands = comb(blank_point, 3)
 
-def bfs():
-    queue=deque()
-    temp_graph=copy.deepcopy(graph)
-    
-    # 바이러스 확인 후 삽입
-    for i in range (n):
-        for j in range (m):
-            if temp_graph[i][j] == 2:
-                queue.append((i,j))
-    
-    # 큐에는 지금 처음 바이러스 위치 다 저장되어있다
-    while queue:
-        x, y = queue.popleft()
+
+# bfs를 통해 바이러스를 확장시킨다 이때 각 후보들의 경우에 대해서 virusmap이 바뀌지 않도록 deeepcopy를 이용한다
+
+def bfs(graph):
+    q= deque(virus_point)
+    while q:
+        x, y = q.popleft()
         for i in range (4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx >= 0 and nx < n and ny >= 0 and ny < m :
-                if temp_graph[nx][ny] == 0:
-                    temp_graph[nx][ny] =2
-                    queue.append((nx, ny))
-
-    global answer # max값을 기록할 지표
-    cnt = 0  
-    for i in range (n):
-        cnt += temp_graph[i].count(0)
-
-    answer =max (answer, cnt)
-
- # 재귀함수를 통해서 모든 지점에 대해서 벽 3개가 세워질 때를 고려할 수 있다
-def makewall (cnt) :
-    # 벽 3개가 세워지면 바이러스를 퍼트리도록 한다 
-    if cnt == 3:
-        # bfs로 바이러스를 퍼트린다
-        bfs()
-        return
-    
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                q.append((nx, ny))
+    # bfs를 통해 바이러스로 감염시킨 후 안전구역의 값을 구한다
+    safe_zone = 0
     for i in range (n):
         for j in range (m):
-            if graph[i][j] ==0: # 빈칸이라면
-                graph[i][j] =1 # 벽을 세우고
-                makewall( cnt + 1) # 벽의 개수를 1개 늘리고 확인한다
-                graph[i][j] = 0 # i,j에 세운 벽을 다시 허문다 
-for i in range (n):
-    graph.append(list(map(int, input().split())))
-answer = 0 # 초기값을 0으로 설정해 갱신
-makewall(0) #초기 벽의 개수를 0 으로 설정해서 3까지 증ㄱ가시킨다다
+            if graph[i][j] == 0:
+                safe_zone += 1
+    # 안전구역의 값을 반환하고
+    return safe_zone
+answer = 0 
+for cand in cands :
+    copy_map = deepcopy(virus_map)
+    # 벽을 세우는 과정
+    for point in cand:
+        a, b = point
+        copy_map[a][b] = 1
+    # 벽을 세운 후 그래프에 bfs를 통해 safezone을 구한다
+    answer = max(answer, bfs(copy_map))
+
 print(answer)
