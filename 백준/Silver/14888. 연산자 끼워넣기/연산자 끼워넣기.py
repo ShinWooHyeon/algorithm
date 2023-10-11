1a0defdc6e1764e5b80358cd8dcc6d674e27673b
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-10-11 18:22:21 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,31 +1,37 @@
-import sys
-n = int(sys.stdin.readline())
-numbers = list(map(int, input().split()))
-add, sub, mul, div = map(int, sys.stdin.readline().split())
-min_value = int(1e9)
-max_value = -int(1e9)
-def dfs(i, now):
-    global min_value, max_value, add, sub, mul, div
-    if i ==n:
-        min_value = min(min_value, now)
-        max_value = max(max_value, now)
+# 저번에도 시간초과로 포기했는데 dfs를 시뮬레이션에 적극 활용할 수 있다
+n = int(input())
+num_list = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+min_value = 1e9
+max_value = -1e9
+
+
+def dfs (i, now):
+    global plus, minus, mul, div, min_value, max_value
+    if i == n :
+        min_value = min(now, min_value)
+        max_value = max(now, max_value)
+    # 남아 있는 기호만큼 쓴다 기호가 없다면 dfs로 확장될 수 없다 
     else:
-        if add > 0:
-            add -= 1
-            dfs(i + 1, now + numbers[i])
-            add += 1
-        if sub > 0:
-            sub -= 1
-            dfs(i + 1, now - numbers[i])
-            sub += 1
+        if plus > 0:
+            # 사용가호 개수 1개 더해주고 거기서 dfs
+            plus -= 1
+            dfs(i + 1, now +num_list[i])
+            # 모든 경우를 다 해보려면 plus를 사용하지 않고 나머지를 사용했을 때도 해봐야 하므로 다시 초기화 시켜야 한다 
+            plus += 1
+        if minus > 0:
+            minus -= 1
+            dfs(i + 1, now - num_list[i])
+            minus += 1
         if mul > 0:
             mul -= 1
-            dfs(i + 1, now * numbers[i])
+            dfs(i + 1, now * num_list[i])
             mul += 1
-        if div > 0:
-            div -= 1
-            dfs(i + 1, int(now / numbers[i]))
+        if div >0:
+            div -=1
+            dfs(i + 1, int(now / num_list[i]))
             div += 1
-dfs(1, numbers[0])
+
+dfs(1, num_list[0])
 print(max_value)
 print(min_value)
