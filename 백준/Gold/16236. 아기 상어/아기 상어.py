b65e0c8ddbed835ba41aedd9b301c82870de39e4
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-10-13 13:37:10 +0900

    [Gold III] Title: 아기 상어, Time: 48 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,72 +1,52 @@
-from collections import deque
+import heapq
 
-N = int(input())
-space = [list(map(int, input().split())) for _ in range(N)]
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range (n)]
+for i in range (n):
+    for j in range (n):
+        if graph[i][j] == 9:
+            graph[i][j] = 0
+            sx = i
+            sy = j
 
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+def bfs():
+    size= 2
+    eat = 0
+    time = 0 
+    q=[]
+    heapq.heappush(q, (0, sx, sy))
+    visited =[[False] * n for _ in range (n)]
+    while q:
+        dist, x, y = heapq.heappop(q)
+        # 자기 장소에 먹을 수 있는 고기가 있다면 (dist 기록하는 건 아래에서)
+        # 큐에서 뽑은건 일반적으로 거리 계산후 최소 거리에 먹이가 있는 경우를 넣은 거니까
+        # 빈칸이 아니면서 물고기려면 0보다 커야한다
+        if 0 < graph[x][y] < size :
+            graph[x][y] = 0
+            eat += 1
+            if eat == size:
+                size += 1
+                eat = 0 
+            # 고기를 먹으러 이동한 것이니까 도착한 순간 q의 정보는 전부 필요가 없어졌고 새로 갱신되야 함
+            # 처음부터 시작하는거니까 즉 기준점의 dist는 다시 0이 되야 한다 
+            time += dist
+            dist =0 
+            q.clear()
+            
+            # 
+            visited =[[False] * n for _ in range (n)]
+        # 고기 찾을 때 까지 1칸씩 이동하면서 그걸 heapq에 넣는 방식으로 진행하는 것이다
+        # 고기 찾았으면 거리 0 근데, 고기 찾으러 한 칸씩 이동중인 큐를 꺼냈다면 기존 dist에 +1을 해야한다 
+        for i in range (4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nd = dist + 1
+            # 방문여부와 이동할 수 있는 칸인지 확인한다
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] <= size and not visited[nx][ny]:
+                heapq.heappush(q, (nd, nx, ny))
+                visited[nx][ny] = True
+    print(time)
 
-pos = []
-for i in range(N):
-    for j in range(N):
-        if space[i][j] == 9:
-            pos.append(i)
-            pos.append(j)
-
-cnt = 0
-
-# 3. 가까운 먹이를 찾는 탐색 문제이기 때문에 `BFS`를 생각해 볼 수 있다.
-# 4. BFS를 사용할 시 입력으로는 현재 아기 상어의 위치를 생각할 수 있고,
-# 	 출력으로는 후보를 리스트를 반환 해야한다.
-def bfs(x, y):
-    visited = [[0]*N for _ in range(N)]
-    queue = deque([[x,y]])
-    cand = []
-
-    visited[x][y] = 1
-
-    while queue:
-        i, j = queue.popleft()
-
-        for idx in range(4):
-            ii, jj = i + dx[idx] , j + dy[idx]
-
-            if 0 <= ii and ii < N and 0 <= jj and jj < N and visited[ii][jj] == 0:
-				# 5. 간선은 상하 좌우지만 조건에 따라서 움직이기 때문에 조건을 상세하여야한다.
-                if space[x][y] > space[ii][jj] and space[ii][jj] != 0:
-                    visited[ii][jj] =  visited[i][j] + 1
-                    cand.append((visited[ii][jj] - 1, ii, jj))
-                elif space[x][y] == space[ii][jj]:
-                    visited[ii][jj] =  visited[i][j] + 1
-                    queue.append([ii,jj])
-                elif space[ii][jj] == 0:
-                    visited[ii][jj] =  visited[i][j] + 1
-                    queue.append([ii,jj])
-                    
-	# 6. 후보 리스트는 우선 순위가 있기 때문에 정렬을 사용할 수 있다.
-    return sorted(cand, key = lambda x: (x[0], x[1], x[2]))
-
-
-i, j = pos
-size = [2, 0]
-# 8. 맨 앞의 후보만 먹고 위치 이동후 다시 BFS 해야한다
-while True:
-    space[i][j] = size[0]
-    cand = deque(bfs(i,j))
-    
-    if not cand:
-        break
-        
-    # 7. 후보리스트가 나오면 맨 앞의 후보 먹이를 뽑아 그 위치로 이동한다.
-    step, xx, yy = cand.popleft()
-    cnt += step
-    size[1] += 1
-    
-	# 9. 몇 개를 먹었는지 몇 초간 이동했는지 체크해 줄 필요가 있다.
-    if size[0] == size[1]:
-        size[0] += 1
-        size[1] = 0
-
-    space[i][j] = 0
-    i, j = xx, yy
-        
-print(cnt)
+bfs()
