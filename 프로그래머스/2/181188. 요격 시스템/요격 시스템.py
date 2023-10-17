e9ac80929f2f5bbfbfbae530b54a4ea4c5d25e8a
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-10-17 13:34:18 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,12 +1,27 @@
-def solution(targets):
-    targets.sort(key = lambda x : (x[1],x[0]))
-    answer = 0
-    s, e = 0 , 0 
-    # target[0] 가 미사일 보다 앞에 있으면 미사일 1개를 더 추가하고 그 위치는  새로운애의 끝
-    # 겹치려면 끝보다 앞에 있어야 하므로 계속 끝에 쏜다고 생각
-    for target in targets:
-        if target[0] >= e : # 미사일보다 뒤에 있어야 겹치는데 안 겹친다는 소리니까
-            e = target[1]
-            answer +=1
-            
-    return answer
+# 1.각 미사일 별로 리스트를 만들고
+# 2. pop해서 그 미사일 없앨 수 있는 모든 애들에 dfs
+# 3. pop한 target을 다시 넣어서 같은 과정 돌입
+# 4. 만약에 targets의 길이가 0이라면 그때 min_value 갱신
+from copy import deepcopy
+
+def solution(targets, result):
+    global min_value
+    a, b = targets.pop() 
+    cands = []
+    for i in range (a, b):
+        cands.append(i)
+    result += 1
+    for cand in cands:
+        targets_cand = deepcopy(targets)
+        for i in len(targets):
+            # 쏜 미사일이 범위안에 있다면 target cand에서 지워버린다 
+            if targets[i][0] <= cand < targets[i][1] :
+                targets_cand.remove(targets[i])
+        # 범위안 미사일을 다 지웠다면, 미사일이 있다면 다음 cand에 대해서 진행한다
+        if len(targets_cand) == 0:
+            min_value = min(result, min_value)
+        else:
+            solution(targets_cand, result)
+
+print(0)
+                
