d7224c5f672a1092d2e4a63f6f76c12d505e7cfd
Shin Woo Hyeon <118239192+ShinWooHyeon@users.noreply.github.com>
2023-10-20 12:28:03 +0900

    [level 2] Title: 호텔 대실, Time: 1.75 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -3,36 +3,25 @@
 # 시가문제의 경우 시간을 전부 분으로 변환시켜준다
 import heapq
 from collections import deque
-from copy import deepcopy
-def check_possible(book_time, k):
-    q =[]
-    if len(book_time) <= k:
-        return True
-    else:
-        for _ in range (k):
-            s, e = book_time.popleft()
-            heapq.heappush(q, e)
-        while book_time:
-            s, e = book_time.popleft()
-            time = heapq.heappop(q)
-            time_ac = time + 10
-            if s >= time_ac:
-                heapq.heappush(q, e)
-            else:
-                return False
-    return True
+
 def solution(book_time):
     answer = 0
     for book in book_time:
         book[0] = int(book[0][:2]) * 60 + int(book[0][3:])
         book[1] = int(book[1][:2]) * 60 + int(book[1][3:])
     book_time = deque(sorted(book_time, key = lambda x: (x[0], x[1])))
-    k = 1
-    while k < len(book_time):
-        check_book = deepcopy(book_time)
-        if check_possible(check_book, k) :
-            return k
+    q =[]
+    answer = 1
+    start, end = book_time.popleft()
+    heapq.heappush(q,end)
+    while book_time:
+        s, e = book_time.popleft()
+        end = heapq.heappop(q)
+        if s >= end + 10: # 이 s 기준으로다 q에 집어넣어 준다 다시'
+            heapq.heappush(q, e)
         else:
-            k += 1
-    
-    return k
+            answer += 1
+            heapq.heappush(q,end)
+            heapq.heappush(q,  e)
+
+    return answer
